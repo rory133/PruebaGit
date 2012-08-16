69fc8ba35a1be0848607692b405e2689d53f0abf
juanma <juan-ma@telefonica.net>
2012-08-16 09:47:25 +0200

    modificacion app

--- src/main/java/com/mkyong/common/App.java
+++ src/main/java/com/mkyong/common/App.java
@@ -7,7 +7,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Maven + Hibernate + MySQL");
+        System.out.println("Maven + Hibernate + MySQL + prueba");
         Session session = HibernateUtil.getSessionFactory().openSession();
         
         session.beginTransaction();
